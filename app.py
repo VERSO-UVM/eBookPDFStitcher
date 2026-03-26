86ff7e065732a1732aeed7a9432ddf6680edbf87
felixwalberg <fwalberg@uvm.edu>
2026-03-26 15:57:14 -0400

    Switched from cookies to flask cachelib session to store user id

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-from flask import Flask, request, render_template, send_file, make_response, redirect,flash
+from flask import Flask, request, render_template, send_file, make_response, redirect, flash, session
+from flask_session import Session
+from cachelib.file import FileSystemCache
+from datetime import timedelta
 import pdf_engine
 import os
 import shutil
 import uuid
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'cachelib'
+app.config['SESSION_SERIALIZATION_FORMAT'] = 'json'
+# threshold represents number of session files it will keep track of at once before deleting old ones
+app.config['SESSION_CACHELIB'] = FileSystemCache(threshold=20, cache_dir="static/session_info")
+Session(app)
+
 upload = "uploaded_files"
 app.secret_key = "Im_only_doing_this_for_flask"
 
+
 @app.route("/")
 def index():
-    if request.cookies.get('id') == None:
+    if(session.get("id") == None):
         id = str(uuid.uuid1())
-        response = make_response(redirect("/"))
-        response.set_cookie('id', id)
-        return response
+        session['id'] = id
+        return redirect("/")
     response = make_response(render_template("index.html"))
-    id = request.cookies.get('id')
+    id = session.get('id')
     upload_dir = os.path.join(upload, id)
     if(not os.path.isdir(upload_dir)):
         os.mkdir(upload_dir)
@@ -30,7 +39,7 @@ def index_buttons():
         files = request.files.getlist("file")
         # save each file in the uploaded_files folder
         # TODO: eventually move to static folder for embedding pdf viewing
-        id = request.cookies.get('id')
+        id = session.get('id')
         for i in files:
             i.save(f"{upload}/{id}/{i.filename}")
         # go to the file settings page
@@ -39,7 +48,7 @@ def index_buttons():
 
 @app.route("/file_settings", methods=["POST"])
 def settings_buttons():
-    id = request.cookies.get('id')
+    id = session.get('id')
     user_directory = os.path.join(upload, id)
     stitched_pdf_dir = os.path.join(user_directory,"stitched_pdfs") 
     file_name = request.form.get("file_name")
@@ -58,7 +67,7 @@ def settings_buttons():
     
 @app.route("/file_settings")
 def auto_stitch():
-    id = request.cookies.get('id')
+    id = session.get('id')
     user_directory = os.path.join(upload, id)
     stitch_dir = os.mkdir(os.path.join(user_directory,"stitched_pdfs"))
     pdf_engine.stitch_pdf(output_folder=stitch_dir,input_directory=user_directory,document_name="auto_stitched")
