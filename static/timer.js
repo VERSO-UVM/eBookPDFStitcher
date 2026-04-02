021bc160fef0f81b66d6df3c24703e82757c6ea0
Fauche-r <fauxcheur597@gmail.com>
2026-04-02 15:51:21 -0400

    Added more time to the timer, timer can now exceed an hour (change the first value in Duration_MS to add more hour)

--- static/timer.js
+++ static/timer.js
@@ -1,7 +1,7 @@
 const TIMER_KEY = 'timerEndTime';
-const DURATION_MS = 30 * 60 * 1000;
+const DURATION_MS = 120 * 60 * 1000;
 
-function startPersistentTimer() {
+function startTimer() {
     let endTime = sessionStorage.getItem(TIMER_KEY);
 
     if (!endTime) {
@@ -17,11 +17,12 @@ function startPersistentTimer() {
         const display = document.getElementById('timer-display');
 
         if (remainingTime >= 0) {
+            const hour =  Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
             if (display) {
-                display.textContent = `${minutes < 10 ? '0' + minutes : minutes}m ${seconds < 10 ? '0' + seconds : seconds}s`;
+                display.textContent = `${hour <10 ? '0' + hour : hour}h ${minutes < 10 ? '0' + minutes : minutes}m ${seconds < 10 ? '0' + seconds : seconds}s`;
             }
         } else {
             clearInterval(interval);
@@ -34,4 +35,4 @@ function startPersistentTimer() {
     }, 1000);
 }
 
-startPersistentTimer();
+startTimer();
