f78b8f865c9eb017c47ef76391513a500ddf4d66
Fauche-r <fauxcheur597@gmail.com>
2026-04-09 23:09:59 -0400

    Begining an implementation of pypandoc

--- format_converter.py
+++ format_converter.py
@@ -2,10 +2,15 @@ import pypandoc
 
 # TODO change the code in this file to a workable one
 # Define input and output files
-input_file = r"pdf_files\Cheat sheet.docx"
+input_file = r"pdf_files\*.docx"
 output_file = r"pdf_files\document.pdf"
 
 output = pypandoc.convert_file(input_file, 'pdf', outputfile=output_file)
 
 if output == "":
     print(f"Successfully converted {input_file} to {output_file}")
+
+def other_format_to_pdf(input_file):
+    
+    output = pypandoc.convert_file(input_file, 'pdf', outputfile=output_file)
+    return output 
