cdb25175cb98e712dc70b28715f71d1ecc68ba31
Fauche-r <fauxcheur597@gmail.com>
2026-04-11 15:21:46 -0400

    Allowed spreadsheet to be inputed, however their formating when converted is strange. Realized the limitation of pypandoc

--- format_converter.py
+++ format_converter.py
@@ -1,17 +1,22 @@
 import pypandoc
 
-
+#Take an input file and convert it to a pdf. Naming convention of the file dont really matter here 
+#as long as it end in .pdf
 def other_format_to_pdf(input_file):
-    print(input_file)
     output_file = input_file  +".pdf"
-    print(output_file)
     pypandoc.convert_file(input_file, 'pdf', outputfile=output_file)
 
 
-# if __name__ == "__main__":
-# # TODO change the code in this file to a workable one
-# # Define input and output files
-#     # input_file = r"pdf_files\*.docx"
-#     # output_file = r"pdf_files\document.pdf"
+#TODO find a python library that allow this, as currently it would seem pypandoc does not allow conversion from pdf
+def pdf_to_other_format(input_file,format):
+    print("wip")
+    # temp_name = input_file[:-4]
+    # output_file = temp_name + "." +format
+    # print(temp_name)
+    # print(output_file)
+    # print(input_file)
+    # pypandoc.convert_file(input_file, format, outputfile=output_file)
 
-#     # output = pypandoc.convert_file(input_file, 'pdf', outputfile=output_file)
+
+if __name__ == "__main__":
+    print("a")
